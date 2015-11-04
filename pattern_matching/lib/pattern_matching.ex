e0a4ad59e99a6ae43c53ff1b6e3e51a6965cc361
Steven Nunez <steven.nunez@gmail.com>
2015-11-04 13:53:05 -0500

    Pattern Matching

--- pattern_matching/lib/pattern_matching.ex
+++ pattern_matching/lib/pattern_matching.ex
@@ -1,2 +1,18 @@
 defmodule PatternMatching do
+  def wookie_name([_,_,%{name: wookie_name},_,_,_]), do: wookie_name
+
+  def embarrass_han([han | _rest]) do
+    Map.put(han, :weight, 200)
+  end
+
+  # def get_humans([%{name: han},_,_,_,%{name: luke}, %{name: boba}]) do
+  def get_humans(characters) do
+    # Enum.filter(characters, &(&1.type == :human)) |> Enum.map(&(&1.name))
+    # [han, luke, boba]
+    for %{type: :human, name: name} <- characters, do: name
+  end
+
+  def get_skinny_folks(characters) do
+    for c <- characters, c.weight < 180, do: c
+  end
 end
--- pattern_matching/test/pattern_matching_test.exs
+++ pattern_matching/test/pattern_matching_test.exs
@@ -2,7 +2,30 @@ defmodule PatternMatchingTest do
   use ExUnit.Case
   doctest PatternMatching
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @characters [
+    %{name: "Han",        type: :human,   rebel: true,    weight: 185},
+    %{name: "Jabba",      type: :hutt,    rebel: false,   weight: 2200},
+    %{name: "Chewie",     type: :wookie,  rebel: true,    weight: 350},
+    %{name: "r2d2",       type: :droid,   rebel: true,    weight: 250},
+    %{name: "Luke",       type: :human,   rebel: true,    weight: 140},
+    %{name: "Boba Fett",  type: :human,   rebel: false,   weight: 175},
+  ]
+
+  test "returns wookie name" do
+    assert PatternMatching.wookie_name(@characters)
+  end
+
+  test "adds weight to Han" do
+    new_han = PatternMatching.embarrass_han(@characters)
+    assert new_han.weight == 200
+  end
+
+  test "get humans" do
+    assert PatternMatching.get_humans(@characters) == ["Han", "Luke", "Boba Fett"]
+  end
+
+  test "get skinny folks" do
+    assert PatternMatching.get_skinny_folks(@characters) ==   [%{name: "Luke",       type: :human,   rebel: true,    weight: 140},
+      %{name: "Boba Fett",  type: :human,   rebel: false,   weight: 175}]
   end
 end
